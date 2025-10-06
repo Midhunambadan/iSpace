c8aafae6299e1a22123a94e45d0037ac60e2793e
Midhun Ambadan <midhunambadan06@gmail.com>
2025-10-06 19:30:18 +0530

    Remove bin/www folder and file(its generate when the time of the initial stage , i use the express generator so thats why its is auto generated, now i remove this file and now the code is run from the app.js file

--- app.js
+++ app.js
@@ -37,27 +37,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(nocache()) 
 
 app.use('/',userRouter)
-
-
 app.use('/admin',adminRouter)
 
 
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const port = process.env.PORT || '7000'
+app.set('port', port);
+app.listen(port,()=>{
+  console.log(`Server is running...on http://localhost:${port}`)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 module.exports = app;
+
